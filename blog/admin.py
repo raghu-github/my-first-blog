a62f18639a41b5a2b5c90116fd464ffa6c6d69e9
Raghu <raghu@gmail.com>
2015-10-30 18:03:28 +0530

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from .models import Tea
-
-admin.site.register(Post)
-admin.site.register(Tea)
 
 # Register your models here.
+admin.site.register(Post)
+
--- blog/models.py
+++ blog/models.py
@@ -8,12 +8,10 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
+	
 	def __str__(self):
 		return self.title
-class Tea(models.Model):
-	category = models.CharField(max_length=200)
-	description = models.TextField()
-	
